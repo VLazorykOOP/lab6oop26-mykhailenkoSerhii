afed272b192cce24d7211e73ac2f9747366b2e07
Mykhailenko Serhi <mykhailenko.serhii@chnu.edu.ua>
2026-05-20 18:20:01 +0300

    Task 1?

--- OOPLab6T/OOPLab6T.cpp
+++ OOPLab6T/OOPLab6T.cpp
@@ -1,10 +1,7 @@
-﻿// OOPLab3Tpl.cpp : Цей файл містить є шаблоном для 
-// виконання лаб. роботи №6. 
-// Він містинь функцію "main" з якої починається та закінчується виконання програми.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <Task3.h>
+#include <Task2.h>
+#include <Task1.h>
 int main()
 {
     std::cout << " Lab #6  !\n";
@@ -12,4 +9,14 @@ int main()
     std::cout << "Task 1: Virtual inheritances" << std::endl;
     std::cout << "Task 2: Virtual functions and classes" << std::endl;
     std::cout << "Task 3: Multiple inheritances" << std::endl;
+    std::cout << "Input what task to run - " << std::endl;
+
+    int choice = 0;
+    std::cin >> choice;
+    if (choice == 1)
+        task1();
+    if (choice == 2)
+        task2();
+    if (choice == 3)
+        task3();
 }
--- OOPLab6T/Task1.cpp
+++ OOPLab6T/Task1.cpp
@@ -1,5 +1,34 @@
-//
-// Created by Smart on 20/05/2026.
-//
-
 #include "Task1.h"
+
+#include <iostream>
+
+class VirtualClass1 { int data1; };
+class VirtualClass2 { int data2; };
+    
+class VirtualClass3 : virtual public VirtualClass1, virtual public VirtualClass2 { int data3; };
+    
+class VirtualClass4 : virtual public VirtualClass3, virtual public VirtualClass2 { int data4; };
+class VirtualClass5 : virtual public VirtualClass3 { int data5; };
+class VirtualClass6 : virtual public VirtualClass2 { int data6; };
+    
+class VirtualClass7 : virtual public VirtualClass4, virtual public VirtualClass5, virtual public VirtualClass6 { int data7; };
+
+class Class1 { int data1; };
+class Class2 { int data2; };
+
+class Class3 : public Class1, public Class2 { int data3; };
+
+class Class4 : public Class3, public Class2 { int data4; };
+class Class5 : public Class3 { int data5; };
+class Class6 : public Class2 { int data6; };
+
+class Class7 : public Class4, public Class5, public Class6 { int data7; };
+
+void task1()
+{
+    Class7 class7 = Class7();
+    VirtualClass7 virtual7 = VirtualClass7();
+
+    std::cout << "Class structure size : " << sizeof(class7) << std::endl;
+    std::cout << "VirtualClass structure size : " << sizeof(virtual7) << std::endl;
+}
